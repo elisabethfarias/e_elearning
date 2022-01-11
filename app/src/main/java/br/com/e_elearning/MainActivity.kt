e2e93fb8b7402bef9aef8e6a146be1c2c4f2c496
Elisabeth Farias <elisabethf@ciandt.com>
2022-01-11 10:58:55 -0300

    Initial commit

---
+++ app/src/main/java/br/com/e_elearning/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.e_elearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
