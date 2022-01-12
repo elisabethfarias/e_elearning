ce1a881c50a4ad4a5cb536000ec33e5977537d5d
Elisabeth Farias <elisabethf@ciandt.com>
2022-01-12 11:07:13 -0300

    Add Data binding library

--- app/src/main/java/br/com/e_elearning/MainActivity.kt
+++ app/src/main/java/br/com/e_elearning/MainActivity.kt
@@ -2,10 +2,12 @@ package br.com.e_elearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.e_elearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
